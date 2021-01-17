e3ffb11ef4054f3a33405d842d52ffa103a0360d
Esma Kızılkaya <38670768+esmakzlkaya@users.noreply.github.com>
2021-01-17 18:35:28 +0300

    Ekle,Sil,Listele metotları yapıldı.

---
+++ MusteriManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        ///Müşteri Listeleme
+
+        public void MusteriListele(params Musteri[] musteriler)
+        {
+            Console.WriteLine("Sisteme kayıtlı kişilerin listesi şu şekildedir : \n");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " - " + musteri.Adi + " - " + musteri.Soyadi + " - " + musteri.Telefon);
+            }
+            Console.WriteLine("\n------------------\n");
+
+        }
+
+        ///Müşteri Ekle
+        
+        public void MusteriEkle(params Musteri[] musteriler) 
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Yeni kişi ' " + musteri.Adi+" "+musteri.Soyadi+" ' başarıyla eklenmiştir.");
+            }
+            Console.WriteLine("\n------------------\n");
+        }
+
+        ///Müşteri Silme
+        
+        public void MusteriSil(params Musteri[] musteriler)
+        {
+            foreach (var silinecekMusteri in musteriler)
+            {
+                Console.WriteLine(silinecekMusteri.Id+" ID nolu müşteri başarıyla silinmiştir.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Esma";
+            musteri1.Soyadi = "Kızılkaya";
+            musteri1.Telefon = "11111111111";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Ali";
+            musteri2.Soyadi = "Veli";
+            musteri2.Telefon = "22222222222";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Zeynep";
+            musteri3.Soyadi = "Demir";
+            musteri3.Telefon = "33333333333";
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriListele(musteri1, musteri2,musteri3);
+
+            musteriManager.MusteriEkle(musteri3,musteri1,musteri2);
+
+            musteriManager.MusteriSil(musteri2,musteri1,musteri3);
         }
     }
 }
